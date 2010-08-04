f0dc4bd0c4b768004430db0a16ce54a49214da57
Kyle Burton <kburton@relaynetwork.com>
2010-08-03 22:56:33 -0400

    bom detection at start of stream for utf 32, 16 and 8

--- src/clj_etl_utils/io.clj
+++ src/clj_etl_utils/io.clj
@@ -1,7 +1,8 @@
 ;; Based off of nu.xom.xinclude.EncodingHeuristics
 
 (ns clj-etl-utils.io
-  (:import [java.io FileInputStream]))
+  (:require [clojure.contrib.logging :as log])
+  (:import [java.io FileInputStream File]))
 
 
 ;; (BufferedReader. (InputStreamReader. (FileInputStream. file) (or encoding *file-encoding*)))
@@ -10,22 +11,115 @@
 (defn first-n-bytes-available [stream n-bytes]
   (let [res (atom [])]
     (try
-     (.mark stream 1024)
+     (if (.markSupported stream)
+       (.mark stream 1024))
      (dotimes [nn n-bytes]
        (let [next-byte (.read stream)]
          (if (not (= -1 next-byte))
            (reset! res (conj @res next-byte)))))
      (finally
-      (.reset stream)))
+      ;; NB: this is no good for already created streams IOW one's
+      ;; that can be recreated, b/c the goal is to advance past the
+      ;; BOM and no further, so we need to consume at most 1 byte at a
+      ;; time
+      (if (.markSupported stream)
+        (.reset stream))))
     @res))
 
+;; from: http://unicode.org/faq/utf_bom.html
+(def *utf-16be*
+     {:encoding  "UTF-16BE"
+      :name      :utf-16be
+      :marker    "\u00FE\u00FF"
+      :marker-bytes [0xFE 0xFF]})
 
-;; TODO: can fail for streams that have too few bytes available for reading
+(def *utf-16le*
+     {:encoding  "UTF-16LE"
+      :name      :utf-16le
+      :marker    "\u00FF\u00FE"
+      :marker-bytes [0xFF 0xFE]})
+
+(def *utf-32be*
+     {:encoding  "UTF-32BE"
+      :name       :utf-32be
+      :marker    "\u0000\u0000\u00FE\u00FF"
+      :marker-bytes [0x00 0x00 0xFE 0xFF]})
+
+(def *utf-32le*
+     {:encoding  "UTF-32LE"
+      :name      :utf-32le
+      :marker    "\u00FF\u00FE\u0000\u0000"
+      :marker-bytes [0xFF 0xFE 0x00 0x00]})
+
+(def *utf-8*
+     {:encoding  "UTF-8"
+      :name      :utf-8
+      :marker    "\u00EF\u00BB\u00BF"
+      :marker-bytes [0xEF 0xBB 0xBF]})
+
+(def *iso-8851-1*
+     {:encoding  "ISO-8859-1"
+      :name      :iso-8859-1
+      :marker    ""
+      :marker-bytes []})
+
+(def *us-ascii*
+     {:encoding  "US-ASCII"
+      :name      :us-ascii
+      :marker    ""
+      :marker-bytes []})
+
+(def *bom-markers*
+     [*utf-32be*
+      *utf-32le*
+      *utf-16be*
+      *utf-16le*
+      *utf-8*])
+
+(def *default-encoding* *iso-8851-1*)
+
+
+(defn byte-marker-matches? [marker-bytes file-bytes]
+  (cond
+    (empty? marker-bytes) false
+    (empty? file-bytes)   false
+    :else
+    (loop [[marker & marker-bytes] marker-bytes
+           [byte & file-bytes]     file-bytes]
+      (cond
+        (or (not marker) (not byte))      true
+        (= marker byte)                   (recur marker-bytes file-bytes)
+        :else                             false))))
+
+;; TODO: what if the stream doesn't support mark?
 ;; TODO: may return a false positive on arbitrary binary data
-(defn open-input-stream [#^String file]
-  (let [is (FileInputStream. file)]
-    (.mark is 1024)
-    (let [[first-byte second-byte third-byte fourth-byte] (first-n-bytes-available is 4)])))
+(defn detect-stream-encoding-via-bom [stream & [default-encoding]]
+  (let [file-bytes (first-n-bytes-available stream 4)]
+    (log/error (format "detect-stream-encoding-via-bom: stream=%s %s" stream file-bytes))
+    (loop [[encoding & encodings] *bom-markers*]
+      (cond
+        (not encoding)
+        ;; TODO: return the default encoding here
+ 	(or default-encoding *default-encoding*)
+        (byte-marker-matches? (:marker-bytes encoding) file-bytes)
+        encoding
+        :else
+        (recur encodings)))))
+
+
+
+
+(defmulti detect-file-encoding-via-bom (fn [x & [default-encoding]] (class x)))
+
+(defmethod detect-file-encoding-via-bom String [file & [default-encoding]]
+  (detect-file-encoding-via-bom (File. file) default-encoding))
+
+(defmethod detect-file-encoding-via-bom File [file & [default-encoding]]
+  (with-open [inp (FileInputStream. file)]
+    (detect-stream-encoding-via-bom inp default-encoding)))
+
+(defmethod detect-file-encoding-via-bom :default [file & [default-encoding]]
+  (throw (format "Error: fell through to :default for detect-stream-encoding-via-bom file=%s" file)))
 
 
 
--- test/clj_etl_utils/io_test.clj
+++ test/clj_etl_utils/io_test.clj
@@ -19,10 +19,59 @@
     (is (= [97]           (io/first-n-bytes-available rdr 1)))
     (is (= [97 97]        (io/first-n-bytes-available rdr 2)))
     (is (= [97 97 97]     (io/first-n-bytes-available rdr 3)))
-    (is (= [97 97 97 97]  (io/first-n-bytes-available rdr 4)))))
+    (is (= [97 97 97 97]  (io/first-n-bytes-available rdr 4))))
+  (with-open [rdr (java.io.FileReader. (fixture-file "sample.in.txt"))]
+    (is (= [76 111 114 101] (io/first-n-bytes-available rdr 4)))))
 
+;; (test-first-n-bytes-available)
 
-'(deftest read-files
-  (let [ascii-txt (fixture-file-contents "sample.in.txt")
-        utf8-txt  (fixture-file-contents "sample.utf-8.txt")]
-    (is (= ascii-txt utf8-txt))))
+(deftest test-byte-marker-matches?
+  (is (not (io/byte-marker-matches? []    [])))
+  (is (not (io/byte-marker-matches? [1]   [0])))
+  (is      (io/byte-marker-matches? [1]   [1]))
+  (is      (io/byte-marker-matches? [1 2] [1 2 3])))
+
+;; (test-byte-marker-matches?)
+
+(deftest test-detect-file-encoding-via-bom
+  (is
+   (= "ISO-8859-1"
+      (:encoding (io/detect-file-encoding-via-bom
+                  (fixture-file "sample.in.txt")))))
+  (is
+   (= "US-ASCII"
+      (:encoding (io/detect-file-encoding-via-bom
+                  (fixture-file "sample.in.txt") io/*us-ascii*))))
+  (is
+   (= "UTF-8"
+      (:encoding (io/detect-file-encoding-via-bom
+                  (fixture-file "sample.utf-8.txt")))))
+  (is
+   (= "UTF-8"
+      (:encoding (io/detect-file-encoding-via-bom
+                  (fixture-file "sample.utf-8.txt") io/*us-ascii*))))
+  (is
+   (= "UTF-16LE"
+      (:encoding (io/detect-file-encoding-via-bom
+                  (fixture-file "sample.utf-16le.txt")))))
+  (is
+   (= "UTF-16BE"
+      (:encoding (io/detect-file-encoding-via-bom
+                  (fixture-file "sample.utf-16be.txt")))))
+  (is
+   (= "UTF-32LE"
+      (:encoding (io/detect-file-encoding-via-bom
+                  (fixture-file "sample.utf-32le.txt")))))
+  (is
+   (= "UTF-32BE"
+      (:encoding (io/detect-file-encoding-via-bom
+                  (fixture-file "sample.utf-32be.txt"))))))
+
+;; (io/detect-file-encoding-via-bom (fixture-file "sample.utf-32be.txt"))
+
+;; (test-detect-file-encoding-via-bom)
+
+;; (deftest read-files
+;;   (let [ascii-txt (fixture-file-contents "sample.in.txt")
+;;         utf8-txt  (fixture-file-contents "sample.utf-8.txt")]
+;;     (is (= ascii-txt utf8-txt))))
