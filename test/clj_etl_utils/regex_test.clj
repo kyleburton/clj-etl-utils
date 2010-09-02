ccc3d42578ac79811a71e8f2ad12088e578c220a
Paul and Kyle <superg@relaynetwork.com>
2010-09-02 19:29:34 -0400

    fixed case sensitivity of regexes

---
+++ test/clj_etl_utils/regex_test.clj
@@ -0,0 +1,26 @@
+(ns regex-test
+  (:require [clj-etl-utils.regex :as rx])
+  (:use [clojure.test]
+        [clj-etl-utils.test-helper]))
+
+
+(defn us-state-match-found [data]
+  (let [regex (:us-states rx/*common-regexes*)]
+    (.matches (.matcher regex data))))
+
+
+(deftest test-us-state-regex
+  (is (not (us-state-match-found "xx")))
+  (is (us-state-match-found "NJ"))
+  (is (us-state-match-found "nJ"))
+  (is (not (us-state-match-found "")))
+  (is (us-state-match-found "pa"))
+  (is (us-state-match-found "NJ"))
+  (is (us-state-match-found "AE"))
+  (is (not (us-state-match-found "not"))))
+
+;; (test-us-state-regex)
+
+
+
+
