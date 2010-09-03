171f0a8b5dc7078b9b3a6279b55b9e3735b46529
Paul and Kyle <superg@relaynetwork.com>
2010-09-03 13:49:25 -0400

    supports post now

--- src/clj_etl_utils/http.clj
+++ src/clj_etl_utils/http.clj
@@ -4,7 +4,8 @@
    [org.apache.commons.httpclient Credentials Header HttpClient UsernamePasswordCredentials NameValuePair]
    [org.apache.commons.httpclient.auth AuthScope]
    [org.apache.commons.httpclient.methods InputStreamRequestEntity PostMethod GetMethod]
-   [org.apache.commons.lang StringUtils]))
+   [org.apache.commons.lang StringUtils]
+   [java.io ByteArrayInputStream]))
 
 ;; TODO: support multiple :basic-auth - support this as a vector of
 ;; maps instead of just a single map, just have this function test
@@ -84,7 +85,7 @@
 
 ;; (defmethod do-get :ua-url-str-and-params [ua url & params]
 ;;   (let [get               (GetMethod. url)
-;;         name-value-pairs  (create-name-value-pairs (second params))]
+;;         name-value-pairs  (map->name-value-pair-vec (second params))]
 ;;     (.setQueryString get (to-array name-value-pairs))
 ;;     (println (format "Return Code: %s" (.executeMethod ua get )))
 ;;     (println (format "Respone: %s" (.getResponeBodyAsString get))))
@@ -107,27 +108,55 @@
 ;; (defmethod do-get :default [& params]
 ;;   (prn "do-get :default"))
 
-(defn create-name-value-pairs [params]
+(defn map->name-value-pair-vec [params]
   (into-array NameValuePair
    (reduce
     (fn [accum key]
-      (cons (NameValuePair. (str key) (params key)) accum))
+      (cons (NameValuePair. (str key) (str (params key))) accum))
     []
     (keys params))))
 
-;;; (create-name-value-pairs { :foo "bar" :chicken "turkey" })
-;;; (create-name-value-pairs {})
+;;; (map->name-value-pair-vec { :foo "bar" :chicken "turkey" })
+;;; (map->name-value-pair-vec {})
 
 
-(defn do-get [ua url & params]
-  (let [get               (GetMethod. url)
-        name-value-pairs  (create-name-value-pairs (second params))]
-    (.setQueryString get name-value-pairs)
-    (printf "do-get: qs=%s" (.getQueryString get))
-    (println (format "Return Code: %s" (.executeMethod (:ua ua) get )))
-    (println (format "Respone: %s" (.getResponseBodyAsString get))))
-  (prn "do-get :ua-url-str-and-params"))
+(defn do-get [#^HttpClient ua #^String url & params]
+  (let [get-method        (GetMethod. url)
+        name-value-pairs  (map->name-value-pair-vec (second params))]
+    (.setQueryString get-method name-value-pairs)
+    (printf "do-get: qs=%s" (.getQueryString get-method))
+    (let [return-code   (.executeMethod (:ua ua) get-method )
+          response-body (.getResponseBodyAsString get-method)]
+      (println (format "Return Code: %s" return-code))
+      (println (format "Respone: %s" response-body ))
+      {:return-code return-code
+       :response-body response-body
+       :http-method get-method})))
 
+
+(defn do-post [#^HttpClient ua #^String url & params]
+  (let [post-method (PostMethod. url)
+        params      (clj-etl-utils.lang/rest-params->map params)]
+    (if (:params params)
+      (.setRequestBody post-method (map->name-value-pair-vec (:params params))))
+    (if (:body params)
+      (do
+        (.setRequestHeader post-method "Content-type" (or (:content-type params) "text/plain; charset=ISO-8859-1"))
+        (.setRequestEntity
+         post-method
+         (InputStreamRequestEntity.
+          (ByteArrayInputStream. (.getBytes (:body params)))
+          (long (.length (:body params)))))))
+    (let [return-code   (.executeMethod (:ua ua) post-method)
+          response-body (.getResponseBodyAsString post-method)]
+      (println (format "Return Code: %s" return-code))
+      (println (format "Respone: %s" response-body ))
+      {:return-code return-code
+       :response-body response-body
+       :http-method post-method})))
+
+
+;;   (do-post (user-agent) "http://localhost:10001" :body "some stuff!")
 
 ;;; (defn do-get [user-agent ])
 
@@ -136,11 +165,14 @@
   @*foo*
 
   (do-get (user-agent) "http://www.google.com" :params {})
+  (do-post (user-agent) "http://localhost:10001" :params { :secret 44 :secret2 49} )
+
+  (do-post (user-agent) "http://localhost:10001" :body "some stuff!")
 
 
-;; http://www.google.com/#hl=en&source=hp&q=yatzee!&aq=f&aqi=g2g-s1g4g-s2g1&aql=&oq=&gs_rfai=CpL9EVhGBTLGjHpaCywTGhYH6BQAAAKoEBU_QNS5J&pbx=1&fp=954aa051d21e9187
+  ;; http://www.google.com/#hl=en&source=hp&q=yatzee!&aq=f&aqi=g2g-s1g4g-s2g1&aql=&oq=&gs_rfai=CpL9EVhGBTLGjHpaCywTGhYH6BQAAAKoEBU_QNS5J&pbx=1&fp=954aa051d21e9187
 
-;; http://www.google.com/#
+  ;; http://www.google.com/#
   (do-get (user-agent) "http://www.google.com/#hl=en&source=hp&q=yatzee!&aq=f&aqi=g2g-s1g4g-s2g1&aql=&oq=&gs_rfai=CpL9EVhGBTLGjHpaCywTGhYH6BQAAAKoEBU_QNS5J&pbx=1&fp=954aa051d21e9187"
           :params)
 
@@ -149,6 +181,6 @@
 
 
 
-)
+  )
 
 
