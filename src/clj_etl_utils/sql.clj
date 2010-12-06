d3c7c82fd3fad9b86f56c3f4bbe5172070d2f0c7
Relay Network <kburton@relaynetwork.com>
2010-12-06 12:42:35 -0500

    added support for sql drop table template

--- src/clj_etl_utils/sql.clj
+++ src/clj_etl_utils/sql.clj
@@ -13,8 +13,22 @@
 (def *dialects* {:postgresql
                  {:templates
                   {:create-table         "/clj_etl_utils/sql/dialects/postgresql/create-table.sql.fleet"
-                   :create-history-table "/clj_etl_utils/sql/dialects/postgresql/create-history-table.sql.fleet"}}})
+                   :create-history-table "/clj_etl_utils/sql/dialects/postgresql/create-history-table.sql.fleet"
+                   :drop-table           "/clj_etl_utils/sql/dialects/postgresql/drop-table.sql.fleet"}}})
 ;; (clj-etl-utils.lang-utils/resource-as-string "/clj_etl_utils/sql/dialects/postgresql/create-table.sql.fleet")
+
+(defn render-drop-table-ddl [dialect template table-name & [params]]
+  (if-not (get *dialects* dialect)
+    (raise "Error: unknown dialect='%s'" dialect))
+  (if-not table-name
+    (raise "Error: can't %s with no name!" template))
+  (let [tmpl-res-url      (or (get-in *dialects* [dialect :templates template])
+                              (raise "Error: no [fleet] template for dialect:%s for %s!" dialect template))
+        tmpl-res          (lang-utils/resource-as-string tmpl-res-url)
+        drop-table-tmpl   (fleet/fleet [params] tmpl-res)]
+    (str (drop-table-tmpl {:table-name table-name
+                           :constraints (get params :constraints )}))))
+
 
 (defn render-create-ddl [dialect template column-defs table-name & [params]]
   (if-not (get *dialects* dialect)
@@ -42,8 +56,11 @@
 (defn create-history-table-ddl [dialect column-defs table-name & [params]]
   (render-create-ddl dialect :create-history-table column-defs table-name params))
 
-(comment
+(defn drop-table-ddl [dialect table-name & [params]]
+  (render-drop-table-ddl dialect :drop-table table-name params))
 
+(comment
+  (render-drop-table-ddl :postgresql :drop-table "pods" & [params])
 
 
 
