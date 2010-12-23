2c1f87866a698dc8b8fc1fe4322b2c671bca767d
Kyle Burton <kburton@relaynetwork.com>
2010-12-23 16:27:22 -0500

    update to 1.0.22

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kyleburton/clj-etl-utils "1.0.20"
+(defproject org.clojars.kyleburton/clj-etl-utils "1.0.22"
   :description "ETL Utilities"
   :url         "http://github.com/kyleburton/clj-etl-utils"
   :license {:name "Eclipse Public License - v 1.0"
