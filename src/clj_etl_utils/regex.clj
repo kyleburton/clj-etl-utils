03a515a0c0b5989ee2d7a06adbd9d71c57cbb398
Kyle Burton <kburton@relaynetwork.com>
2011-04-04 14:33:43 -0400

    re-grouped std regexes, added Canadian postal code regex

--- src/clj_etl_utils/regex.clj
+++ src/clj_etl_utils/regex.clj
@@ -1,41 +1,80 @@
-(ns
-    ^{:doc "Collection of commonly used regular expressions."
-      :author "Kyle Burton"}
-  clj-etl-utils.regex
-  (:import
-   [java.util.regex Pattern Matcher])
-  (:require
-   [clojure.contrib.str-utils :as str]
-   [clj-etl-utils.ref-data :as ref-data]))
+(ns ^{:doc "Collection of commonly used regular expressions."
+      :author "Kyle Burton"} clj-etl-utils.regex
+      (:import
+       [java.util.regex Pattern Matcher])
+      (:require
+       [clojure.contrib.str-utils :as str]
+       [clj-etl-utils.ref-data :as ref-data]))
+
+;; *ns*
 
 ;; regexes, initial set pulled from Regex::Common CPAN module
 (def *common-regexes*
-  {:num-real #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[0123456789]|[.])(?:[0123456789]*)(?:(?:[.])(?:[0123456789]{0,}))?)(?:(?:[E])(?:(?:[+-]?)(?:[0123456789]+))|)))"
-   :num-int #"(?xism:(?:(?:[+-]?)(?:[0123456789]+)))"
-   :num-decimal #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[0123456789]|[.])(?:[0123456789]*)(?:(?:[.])(?:[0123456789]{0,}))?)))"
-   :num-hex #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[0123456789ABCDEF]|[.])(?:[0123456789ABCDEF]*)(?:(?:[.])(?:[0123456789ABCDEF]{0,}))?)(?:(?:[G])(?:(?:[+-]?)(?:[0123456789ABCDEF]+))|)))"
-   :num-dec #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[0123456789]|[.])(?:[0123456789]*)(?:(?:[.])(?:[0123456789]{0,}))?)(?:(?:[E])(?:(?:[+-]?)(?:[0123456789]+))|)))"
-   :num-oct #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[01234567]|[.])(?:[01234567]*)(?:(?:[.])(?:[01234567]{0,}))?)(?:(?:[E])(?:(?:[+-]?)(?:[01234567]+))|)))"
-   :num-bin #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[01]|[.])(?:[01]*)(?:(?:[.])(?:[01]{0,}))?)(?:(?:[E])(?:(?:[+-]?)(?:[01]+))|)))"
-   :num-roman #"(?xism:(?xi)(?=[MDCLXVI])
+     {:numeric
+      {:real #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[0123456789]|[.])(?:[0123456789]*)(?:(?:[.])(?:[0123456789]{0,}))?)(?:(?:[E])(?:(?:[+-]?)(?:[0123456789]+))|)))"
+       :int #"(?xism:(?:(?:[+-]?)(?:[0123456789]+)))"
+       :dec     #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[0123456789]|[.])(?:[0123456789]*)(?:(?:[.])(?:[0123456789]{0,}))?)(?:(?:[E])(?:(?:[+-]?)(?:[0123456789]+))|)))"
+       :decimal #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[0123456789]|[.])(?:[0123456789]*)(?:(?:[.])(?:[0123456789]{0,}))?)))"
+       :hex #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[0123456789ABCDEF]|[.])(?:[0123456789ABCDEF]*)(?:(?:[.])(?:[0123456789ABCDEF]{0,}))?)(?:(?:[G])(?:(?:[+-]?)(?:[0123456789ABCDEF]+))|)))"
+       :oct #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[01234567]|[.])(?:[01234567]*)(?:(?:[.])(?:[01234567]{0,}))?)(?:(?:[E])(?:(?:[+-]?)(?:[01234567]+))|)))"
+       :bin #"(?xism:(?:(?i)(?:[+-]?)(?:(?=[01]|[.])(?:[01]*)(?:(?:[.])(?:[01]{0,}))?)(?:(?:[E])(?:(?:[+-]?)(?:[01]+))|)))"
+       :roman #"(?xism:(?xi)(?=[MDCLXVI])
                          (?:M{0,3}
                             (D?C{0,3}|CD|CM)?
                             (L?X{0,3}|XL|XC)?
-                            (V?I{0,3}|IV|IX)?))"
-   :zip #"(?xism:(?:(?:(?:USA?)-){0,1}(?:(?:(?:[0-9]{3})(?:[0-9]{2}))(?:(?:-)(?:(?:[0-9]{2})(?:[0-9]{2}))){0,1})))"
-   :net-ipv4 #"(?xism:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})))"
-   :net-mac #"(?xism:(?:(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2})))"
-   :net-domain #"(?xism:(?: |(?:[A-Za-z](?:(?:[-A-Za-z0-9]){0,61}[A-Za-z0-9])?(?:\.[A-Za-z](?:(?:[-A-Za-z0-9]){0,61}[A-Za-z0-9])?)*)))"
-   :phone #"(?:1[- ]?)?\(?[2-9]\d{2}\)?[-\. ]?\d{3}[-\. ]?\d{4}(?:\s*(?:e|ex|ext|x|xtn|extension)?\s*\d*)"
-   :us-states        (Pattern/compile (format "(?xism:%s)" (str/str-join "|" (map first ref-data/*us-states*))))
-   :us-state-names   (Pattern/compile (format "(?xism:%s)" (str/str-join "|" (map second ref-data/*us-states*))))
-   :us-airport-codes (Pattern/compile (format "(?xism:%s)" (str/str-join "|" (map #(nth % 2) ref-data/*us-airport-codes*))))
-   :us-area-codes    (Pattern/compile (format "(?xism:%s)" (str/str-join "|" ref-data/*us-area-codes*)))
+                            (V?I{0,3}|IV|IX)?))"}
+      :geographic
+      {:iso
+       {:country-3 (Pattern/compile
+                    (format
+                     "(?xism:%s)"
+                     (str/str-join
+                      "|"
+                      (map first ref-data/*iso-3-country-codes*))))
+
+        :country-2 (Pattern/compile
+                    (format
+                     "(?xism:%s)"
+                     (str/str-join
+                      "|"
+                      (map first ref-data/*iso-2-country-codes*))))}
+       :usa
+       {:zip #"(?xism:(?:(?:(?:USA?)-){0,1}(?:(?:(?:[0-9]{3})(?:[0-9]{2}))(?:(?:-)(?:(?:[0-9]{2})(?:[0-9]{2}))){0,1})))"
+        ;; NB: same as zip, just using a consistent name
+        :postal-code #"(?xism:(?:(?:(?:USA?)-){0,1}(?:(?:(?:[0-9]{3})(?:[0-9]{2}))(?:(?:-)(?:(?:[0-9]{2})(?:[0-9]{2}))){0,1})))"
+        :state        (Pattern/compile (format "(?xism:%s)" (str/str-join "|" (map first ref-data/*us-states*))))
+        :state-name   (Pattern/compile (format "(?xism:%s)" (str/str-join "|" (map second ref-data/*us-states*))))
+        :airport-code (Pattern/compile (format "(?xism:%s)" (str/str-join "|" (map #(nth % 2) ref-data/*us-airport-codes*))))
+        :area-code    (Pattern/compile (format "(?xism:%s)" (str/str-join "|" ref-data/*us-area-codes*)))
+        :phone #"(?:1[- ]?)?\(?[2-9]\d{2}\)?[-\. ]?\d{3}[-\. ]?\d{4}(?:\s*(?:e|ex|ext|x|xtn|extension)?\s*\d*)"}
+
+       :can
+       {:postal-code #"[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?\d[ABCEGHJKLMNPRSTVWXYZ]\d"}}
+
+      :internet
+      {:ipv4 #"(?xism:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})[.](?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})))"
+       :mac #"(?xism:(?:(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2}):(?:[0-9a-fA-F]{1,2})))"
+       :net-domain #"(?xism:(?: |(?:[A-Za-z](?:(?:[-A-Za-z0-9]){0,61}[A-Za-z0-9])?(?:\.[A-Za-z](?:(?:[-A-Za-z0-9]){0,61}[A-Za-z0-9])?)*)))"}
+      :general
+      {:word           #"(?:[\w-]+)"
+       :punctuation    #"(?:[\.,\?/'\";:\\`~!\(\)]+)"}})
 
 
-   :word           #"(?:[\w-]+)"
-   :punctuation    #"(?:[\.,\?/'\";:\\`~!\(\)]+)"})
+(comment
+  (:zip *common-regexes*)
 
+  )
+
+(defn std-regex [& path]
+  (get-in *common-regexes* path))
+
+(defn std-regex-compose [& paths]
+  (Pattern/compile (format "(?:%s)"
+                           (str/str-join
+                            "|"
+                            (map (fn [path]
+                                   (apply std-regex path))
+                                 paths)))))
 
 (defn all-groups
   "Extracts all the groups from a java.util.regex.Matcher into a seq."
--- test/clj_etl_utils/regex_test.clj
+++ test/clj_etl_utils/regex_test.clj
@@ -5,7 +5,7 @@
 
 
 (defn us-state-match-found [data]
-  (let [regex (:us-states rx/*common-regexes*)]
+  (let [regex (rx/std-regex :geographic :usa :state)]
     (.matches (.matcher regex data))))
 
 
@@ -21,6 +21,87 @@
 
 ;; (test-us-state-regex)
 
+(def *sample-postal-codes*
+     {:can
+      ["R2K 3X4"
+       "R6W 4B7"
+       "L1T 2W2"
+       "M2R 1L7"
+       "R5G 0M3"
+       "R2V4J1"
+       "H9S2R9"
+       "H1T 2R6"
+       "H1T 2R6"
+       "N0G 2H0"
+       "H1P 1K5"
+       "L1V 6L4"
+       "T2H 1K2"
+       "L8H 3B2"
+       "H1T 2R6"
+       "L8E0C7"
+       "J7R 6W2"
+       "J3H 0A9"
+       "R0G 2B0"
+       "L4Z 2X2"
+       "N7T 8B2"
+       "V2X 2R4"
+       "T6K 3P8"
+       "V0G 1M0"
+       "N8H 3P2"
+       "G6E 1A2"
+       "J3X 2B8"]
+      :usa
+      ["93306"
+       "94568"
+       "26547"
+       "12440"
+       "43333"
+       "16692"
+       "98392"
+       "26038"
+       "90815"
+       "67484"
+       "63366"
+       "60946"
+       "61048"
+       "24950"
+       "07036"
+       "65764"
+       "12538"
+       "30296"
+       "41601"
+       "75228"
+       "26267"
+       "84320"
+       "61743"
+       "37402"
+       "94523"]})
 
 
+(deftest test-usa-zip-matcher
+  (doseq [zip5 (:usa *sample-postal-codes*)]
+    (is (re-matches (rx/std-regex :geographic :usa :zip)
+                    zip5))))
 
+(deftest test-can-postal-code-matcher
+  (doseq [postal-code (:can *sample-postal-codes*)]
+    (is (re-matches (rx/std-regex :geographic :can :postal-code)
+                    postal-code))))
+
+(deftest test-north-america-postal-code-matcher
+  (let [regex (rx/std-regex-compose [:geographic :usa :postal-code]
+                                    [:geographic :can :postal-code])]
+    (doseq [postal-code (concat
+                         (:can *sample-postal-codes*)
+                         (:usa *sample-postal-codes*))]
+      (is (re-matches regex postal-code)))))
+
+(comment
+
+  (apply rx/std-regex [:geographic :can :postal-code])
+
+  (test-usa-zip-matcher)
+  (test-can-postal-code-matcher)
+  (test-north-america-postal-code-matcher)
+
+  )
