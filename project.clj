abc8a812bb0530f7e5eddc063a7bade0fc9bb1c3
Paul and Kyle <kburton@relaynetwork.com>
2011-07-13 17:15:03 -0400

    add marginalia doc tool

--- project.clj
+++ project.clj
@@ -1,28 +1,29 @@
-(defproject org.clojars.kyleburton/clj-etl-utils "1.0.37"
+(defproject org.clojars.kyleburton/clj-etl-utils "1.0.38-SNAPSHOT"
   :description "ETL Utilities"
   :url         "http://github.com/kyleburton/clj-etl-utils"
-  :license {:name "Eclipse Public License - v 1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"
+  :license {:name         "Eclipse Public License - v 1.0"
+            :url          "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
-            :comments "same as Clojure"}
-  :java-source-path [["java"]]
-  :warn-on-reflection true
-  :jvm-opts ["-Xmx512M"]
-  :dev-dependencies [[swank-clojure "1.3.0"]
-                     ;;[autodoc "0.7.1"]
-                     ]
-  :dependencies [[org.clojure/clojure "1.2.0"]
+            :comments     "Same as Clojure"}
+  :java-source-path     [["java"]]
+  :warn-on-reflection   true
+  :local-repo-classpath true
+  :jvm-opts             ["-Xmx512M"]
+  :dev-dependencies [[swank-clojure   "1.3.0"]
+                   ;;[autodoc       "0.7.1"]
+                     [lein-marginalia "0.6.0"]]
+  :dependencies [[org.clojure/clojure         "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [commons-io "2.0"]
-                 [fleet "0.9.3"]
-                 [log4j/log4j "1.2.14"]
+                 [commons-io          "2.0"]
+                 [fleet               "0.9.3"]
+                 [log4j/log4j         "1.2.14"]
                  [org.mindrot/jbcrypt "0.3m"]
-                 [commons-httpclient "3.1"]
-                 [commons-codec "1.4"]
-                 [commons-lang "2.5"]]
+                 [commons-httpclient  "3.1"]
+                 [commons-codec       "1.4"]
+                 [commons-lang        "2.5"]]
   :autodoc {
-    :name "clj-etl-utils"
-    :page-title "clj-etl-utils: API Documentation"
+    :name        "clj-etl-utils"
+    :page-title  "clj-etl-utils: API Documentation"
     :description "ETL Utilites for Clojure"
-    :web-home "http://kyleburton.github.com/projects/clj-etl-utils/"
+    :web-home    "http://kyleburton.github.com/projects/clj-etl-utils/"
   })
