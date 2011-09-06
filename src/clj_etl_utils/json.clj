52eaf1f961fdee7eeda884d275bd527e3e82f790
Paul and Kyle <kburton@relaynetwork.com>
2011-09-06 11:30:03 -0400

    must return valid json

--- src/clj_etl_utils/json.clj
+++ src/clj_etl_utils/json.clj
@@ -7,8 +7,18 @@
 
 (extend java.sql.Timestamp Write-JSON
         {:write-json (fn [x #^PrintWriter out]
-                       (.print out (.toString x)))})
+                       (.print out "\"")
+                       (.print out (.toString x))
+                       (.print out "\""))})
+
+(extend java.util.Date Write-JSON
+        {:write-json (fn [x #^PrintWriter out]
+                       (.print out "\"")
+                       (.print out (.toString x))
+                       (.print out "\""))})
 
 (extend clojure.lang.Fn Write-JSON
         {:write-json (fn [x #^PrintWriter out]
-                       (.print out (.toString x)))})
+                       (.print out "\"")
+                       (.print out (.toString x))
+                       (.print out "\""))})
