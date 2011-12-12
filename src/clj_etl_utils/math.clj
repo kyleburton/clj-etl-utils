ab04244a29cd07680724ad2e592b1fcf5a019e2d
Josh <eng@relaynetwork.com>
2011-12-12 15:55:47 -0500

    base62 encoding of largeish integer like things

--- src/clj_etl_utils/math.clj
+++ src/clj_etl_utils/math.clj
@@ -24,3 +24,44 @@
 
   )
 
+(def base62-digits "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+
+(defn base62-encode [n & [base]]
+  (let [#^BigInteger base (BigInteger/valueOf (or base 62))
+        #^BigInteger n (if (isa? (class n) BigInteger)
+                         n
+                         (BigInteger/valueOf n))]
+    (if (= -1 (.compareTo n BigInteger/ZERO))
+      (raise "Error: negative numbers are not supported, sorry: %s" n))
+    (loop [res                 (StringBuilder.)
+           [number remainder]  (.divideAndRemainder n base)]
+      (println (format "res:%s number:%s/%s remainder:%s" res number (class number) remainder))
+      (if (= 1 (.compareTo number BigInteger/ZERO))
+        (recur
+         (.insert res 0 (.charAt base62-digits (.intValue remainder)))
+         (.divideAndRemainder number base))
+        (let [res (if (= 1 (.compareTo remainder BigInteger/ZERO))
+                    (str (.insert res 0 (.charAt base62-digits (.intValue remainder))))
+                    (str res))]
+          (if (empty? res)
+            "0"
+            res))))))
+
+
+(defn base62-decode [#^String s & [base]]
+  (let [#^BigInteger base (BigInteger/valueOf (or base 62))
+        digits (count s)]
+    (if (empty? s)
+      BigInteger/ZERO
+      (loop [res #^BigInteger BigInteger/ZERO
+             idx 0]
+        (if (>= idx digits)
+          res
+          (let [c     (.charAt s (- digits idx 1))
+                digit (BigInteger/valueOf (.indexOf base62-digits (str c)))]
+            (recur (.add res (.multiply digit (.pow base idx)))
+                   (inc idx))))))))
+
+
+
