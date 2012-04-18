7e83b900e6969140d66ac9eed5fc9e36285d52d7
Josh and Kyle <eng@relaynetwork.com>
2012-04-18 15:16:04 -0400

    1.0.46 includes json timezone information for sql timestamps

--- src/clj_etl_utils/json.clj
+++ src/clj_etl_utils/json.clj
@@ -1,14 +1,18 @@
 (ns clj-etl-utils.json
   (:import
    [java.sql Timestamp]
-   [java.io PrintWriter])
+   [java.io PrintWriter]
+   [org.joda.time DateTime  ]
+   [org.joda.time.format ISODateTimeFormat])
   (:use
    [clojure.contrib.json  :only [Write-JSON]]))
 
 (extend java.sql.Timestamp Write-JSON
         {:write-json (fn [x #^PrintWriter out]
                        (.print out "\"")
-                       (.print out (.toString x))
+                       (.print out
+                               (.print (ISODateTimeFormat/dateTime )
+                                       (org.joda.time.DateTime. x)))
                        (.print out "\""))})
 
 (extend org.joda.time.DateTime Write-JSON
@@ -28,3 +32,17 @@
                        (.print out "\"")
                        (.print out (.toString x))
                        (.print out "\""))})
+
+
+
+(comment
+  (.print (ISODateTimeFormat/dateTime ) (org.joda.time.DateTime. ))
+
+  "2012-04-18T14:33:02.068-04:00"
+
+
+  (rn.clorine.core/with-connection :wall_ro
+    (.print
+     (ISODateTimeFormat/dateTime )
+     (org.joda.time.DateTime. (:created_at (rn-db.core/find-first-by :wall.actions {:id 5664})))))
+  )
