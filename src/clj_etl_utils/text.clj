c1604801eb67b62f77f8c9c0093e73646df9dc03
Kyle Burton <kyle.burton@gmail.com>
2013-04-26 16:00:13 -0400

    fix md5 fn

--- src/clj_etl_utils/text.clj
+++ src/clj_etl_utils/text.clj
@@ -38,9 +38,9 @@ create or clean up the actual temporary file itself.
   md5->string [bytes]
   (let [digester (java.security.MessageDigest/getInstance "MD5")]
     (.update digester bytes)
-    (apply str (map (fn [byte]
-                      (Integer/toHexString (bit-and 0xFF byte)))
-                    (.digest digester)))))
+    (.toString
+      (java.math.BigInteger. 1 (.digest digester))
+      16)))
 
 (defn
   ^{:doc "Compute the SHA1 sum of a byte buffer, returning it as a hex-encoded string."}
