02e99d1966a041648f1f248c4b6efd3b517c67dc
Kyle Burton <kyle.burton@gmail.com>
2013-07-17 17:51:58 -0400

    lein-release plugin: preparing 1.0.67 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kyleburton/clj-etl-utils "1.0.67-SNAPSHOT"
+(defproject org.clojars.kyleburton/clj-etl-utils "1.0.67"
   :description "ETL Utilities"
   :url         "http://github.com/kyleburton/clj-etl-utils"
   :license {:name         "Eclipse Public License - v 1.0"
