73612cba6009a0db57ef007c1a3a6c5644e4c5ae
Kyle Burton <kyle.burton@gmail.com>
2013-07-20 12:43:14 -0400

    search index file

--- src/clj_etl_utils/indexer.clj
+++ src/clj_etl_utils/indexer.clj
@@ -7,8 +7,11 @@
   (:require [clojure.contrib.duck-streams :as ds]
             [clojure.contrib.shell-out    :as sh]
             [clj-etl-utils.sequences      :as sequences]
-            [clj-etl-utils.io             :as io])
-  (:import [java.io RandomAccessFile]))
+            [clj-etl-utils.io             :as io]
+            [clojure.java.io              :as cljio])
+  (:import
+   [java.io RandomAccessFile FileInputStream InputStreamReader BufferedReader]
+   [org.apache.commons.io.input BoundedInputStream]))
 
 ;; index line oriented files
 
@@ -50,7 +53,7 @@ For all the lines in the file.
             (fn [line]
               [(str (.charAt line 0))])))
 
-)
+  )
 
 ;; returns a sequnce of [key line-start-byte-pos line-endbyte-pos]
 ;; given a key-fn that takes a line of text and returns a string key that represents the line.
@@ -76,7 +79,7 @@ For all the lines in the file.
         true
         (do
           (doseq [val kvals]
-           (.println outp (format "%s\t%s\t%s" val start end)))
+            (.println outp (format "%s\t%s\t%s" val start end)))
           (recur vals))))))
 
 ;; NB: return value isn't taking into account error statuses
@@ -84,8 +87,9 @@ For all the lines in the file.
 (defn sort-index-file [#^String index-file]
   (let [tmp    (java.io.File/createTempFile "idx-srt" "tmp")
         tmpnam (.getName tmp)]
-   (sh/sh "sort" "-o" tmpnam index-file)
-   (sh/sh "mv" tmpnam index-file))
+    (sh/sh "sort" "-o" tmpnam index-file
+           :env {"LANG" "C"})
+    (sh/sh "mv" tmpnam index-file))
   true)
 
 (defn index-file! [#^String input-file #^String index-file #^IFn key-fn]
@@ -97,7 +101,7 @@ For all the lines in the file.
 
   (index-file! "file.txt" ".file.txt.id-idx" #(first (.split % "\t")))
 
-)
+  )
 
 ;; TODO: this is splitting multiple times, rework to only split 1x
 (defn index-blocks-seq [#^String index-file]
@@ -125,7 +129,7 @@ For all the lines in the file.
 (comment
   (index-blocks-seq ".file.txt.id-idx")
   ((["1" 24 48]) (["2" 48 65]) (["3" 65 88]) (["99" 0 24] ["99" 88 115]))
-)
+  )
 
 
 (defn records-for-idx-block #^String [inp-file idx-block]
@@ -142,7 +146,7 @@ For all the lines in the file.
 
   (records-for-idx-block "file.txt" [["99" 0 24] ["99" 88 115]])
 
-)
+  )
 
 ;; TODO: building an index has to stream the records, if we are to
 ;; build N indicies we will have to stream the records N times, modify
@@ -161,24 +165,201 @@ index values returning records from the data file."
 
   (record-blocks-via-index "file.txt" ".file.txt.id-idx")
 
-)
-
-;; TODO: Implement mutiple index block streaming (grouping across
-;; mutiple data files).
-
-(comment
-
-
-  (index-file! "file.txt" ".file.txt.id-idx" #(first (.split % "\t")))
-  (record-blocks-via-index "file.txt" ".file.txt.id-idx")
-
   )
 
-;; TODO: Implement the binary search on the index
+;; 1 MB
+(def min-streaming-threshold (* 1024 1024 1))
+
+;; matcher = (fn [index-val term]) => bool
+(defn index-search
+  ([idx-file term]
+     (index-search idx-file term =))
+  ([idx-file term matcher]
+     (with-open [rdr (cljio/reader idx-file)]
+       (index-search idx-file term matcher rdr)))
+  ([idx-file term matcher rdr]
+     (loop [line (.readLine rdr)
+            res  []]
+       (if (nil? line)
+         res
+         (let [[v s e] (.split line "\t" 3)
+               direction (compare v term)]
+           (cond
+             (matcher v term)
+             (recur (.readLine rdr)
+                    (conj res [v (Long/parseLong s) (Long/parseLong e)]))
+
+             (pos? direction)
+             (do
+               #_(println (format "direction was positive, indicating we've gone past: (compare \"%s\" \"%s\") %d"
+                                v term direction))
+               res)
+
+             :continue
+             (recur (.readLine rdr) res)))))))
+
+;; NB: this only works with \n (byte value 10) as a line separator
+(defn rewind-to-newline [^RandomAccessFile fp min-pos]
+  #_(println "rewind-to-newline: remove the max iters")
+  (loop [] ;; max-iters 10000
+    (let [b  (.readByte fp)
+          ch (Byte/toString b)]
+      (cond
+        ;; (<= max-iters 0)
+        ;; (raise "Too much recursion")
+
+        (= (.intValue b) 10)
+        (do
+          #_(println (format "rewind-to-newline: Found newline at %d" (.getFilePointer fp)))
+          true)
+
+        (<= (.getFilePointer fp) min-pos)
+        (do
+          #_(println (format "rewind-to-newline: Rewound to start"))
+          (.seek fp 0)
+          true)
+
+        :rewind
+        (do
+          #_(println (format "rewind-to-newline: [%d/%s] Did not find newline at %d, going back to %d"
+                           (.intValue b)
+                           ch
+                           (.getFilePointer fp)
+                           (- (.getFilePointer fp) 2)))
+          (.seek fp (- (.getFilePointer fp) 2))
+          ;; (recur (dec max-iters))
+          (recur))))))
+
+
+;; spos must point at either the start of the file, or the beginning of a line
+;; epos must point at either the end of the file, or a newline
+(defn index-search-prefix-impl [^String idx-file ^String term spos epos]
+  #_(println (format "index-search-prefix-impl %s %s %d %d"
+                   idx-file term spos epos))
+  (if (<= (- epos spos) min-streaming-threshold)
+    (with-open [rdr (BufferedReader.
+                     (InputStreamReader.
+                      (BoundedInputStream.
+                       (doto (FileInputStream. idx-file)
+                         (.skip spos))
+                       (- epos spos))))]
+      #_(println (format "before binary search, spos=%d to epos=%d under THRESH, falling back to streaming search" spos epos))
+      (index-search idx-file
+                    term
+                    (fn [idx-val term]
+                      (.startsWith idx-val term))
+                    rdr))
+    (with-open [fp (RandomAccessFile. idx-file "r")]
+      (loop [;;max-iters 25
+             spos   spos
+             epos   epos
+             middle (long (/ (- epos spos) 2))]
+        #_(println (format "loop: spos=%d epos=%d middle=%d" spos epos middle))
+        ;; (when (<= max-iters 0)
+        ;;   (raise "too much recursion"))
+        (.seek fp middle)
+        (rewind-to-newline fp spos)
+        (let [middle  (.getFilePointer fp)
+              line    (.readLine fp)
+              [iterm bstart bend] (.split line "\t" 3)
+              order   (compare term iterm)]
+          #_(println (format "Looking at[%d] line=%s"
+                           (.getFilePointer fp)
+                           line))
+          (cond
+            (<= (- epos spos) min-streaming-threshold)
+            (with-open [rdr (BufferedReader.
+                             (InputStreamReader.
+                              (BoundedInputStream.
+                               (doto (FileInputStream. idx-file)
+                                 (.skip spos))
+                               (- epos spos))))]
+              #_(println (format "in binary search, spos=%d to epos=%d under THRESH, falling back to streaming search" spos epos))
+              (index-search
+               idx-file
+               term
+               (fn [idx-val term]
+                 #_(println (format "(.startsWith \"%s\" \"%s\") => %s"
+                                  idx-val term (.startsWith idx-val term)))
+                 (.startsWith idx-val term))
+               rdr))
+
+            (neg? order)
+            (do
+              #_(println (format "order was: %d, go left" order))
+              (recur ;; (dec max-iters)
+               spos
+               middle
+               (long (- middle  (/ (- middle spos) 2)))))
+
+            (zero? order)
+            (do
+              #_(println (format "order was: %d, we're in the block, need to find the start" order)))
+
+            (pos? order)
+            (do
+              #_(println (format "order was: %d, go right" order))
+              (recur
+               ;; (dec max-iters)
+               middle
+               epos
+               (long (- epos  (/ (- epos middle) 2)))))))))))
+
+
+(defn index-search-prefix [^String idx-file ^String term]
+  ;; binary search the file
+  ;; open a RandomAccessFile
+  ;; start=0 end=LEN
+  ;; if (- end len) < THRESH, just stream through the section
+  ;; jump to the middle, rewind to '\n'
+  (let [epos (.length (java.io.File. idx-file))]
+    (if (<= epos min-streaming-threshold)
+      (do
+        #_(println (format "file size %d < thresh %d, falling back to streaming"
+                         epos min-streaming-threshold))
+        (index-search idx-file term #(= %1 %2)))
+      (index-search-prefix-impl idx-file term 0 epos))))
+
 
 (comment
 
-  (index-search "file.txt" ".file.txt.id-idx" "99")
+ (index-search-prefix
+  "tmp/.free-zipcode-database.csv.city-idx"
+  "NEW PHILA")
 
-)
+ (defn sort-compatible-compare [s1 s2]
+   (let [len (min (count s1) (count s2))]
+     (loop [idx 0]
+       (cond
+         (= idx len)
+         0
+         :compare-chars
+         (let [c1 (.charAt s1 idx)
+               c2 (.charAt s1 idx)])))))
+
+ (compare "A" " ")
+
+ (compare "NEWAGEN" "NEW PHILA")
+
+
+ (.compareTo "NEWAGEN" "NEW PHILA")
+
+
+
+ )
+
+
+;; TODO: take the result of index-search and look up the records out of the original file
+
+;; TODO: Implement mutiple index block streaming (grouping across
+;; mutiple data files to create candidate clusters).
+
+(comment
+
+
+  (index-file! "file.txt" ".file.txt.id-idx" (fn [line] [(first (.split line "\t"))]))
+  (record-blocks-via-index "file.txt" ".file.txt.id-idx")
+
+
+  )
 
