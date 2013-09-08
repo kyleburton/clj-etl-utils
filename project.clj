5a2075f0bdbffb56ebe24fbd26c6ed1ff49c86b2
Kyle Burton <kyle.burton@gmail.com>
2013-09-07 21:25:18 -0400

    support lein2, test vs clojures 1.2 through 1.6

--- project.clj
+++ project.clj
@@ -5,27 +5,28 @@
             :url          "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments     "Same as Clojure"}
-  :java-source-path     "java"
-  ;; :warn-on-reflection   true
-  :local-repo-classpath true
-  :jvm-opts             ["-Xmx512M"]
-  :plugins [[lein-release/lein-release "1.0.0"]]
   :lein-release {:deploy-via :clojars}
-  :dev-dependencies [[swank-clojure   "1.4.3"]
-                   ;;[autodoc       "0.7.1"]
-                     [lein-marginalia "0.6.0"]]
-  :dependencies [[org.clojure/clojure         "1.5.1"]
-                [org.clojure/java.classpath "0.2.0"]
-                [org.clojure/core.incubator "0.1.0"]
-                [org.clojure/data.json      "0.2.0"]
-                [org.clojure/tools.logging  "0.2.3"]
-                [commons-io          "2.0"]
-                [log4j/log4j         "1.2.14"]
-                [org.mindrot/jbcrypt "0.3m"]
-                [commons-httpclient  "3.1"]
-                [commons-codec       "1.4"]
-                [commons-lang        "2.5"]
-                [joda-time/joda-time                   "2.2"]]
+  :java-source-path     "java"
+  :repositories         {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+
+  :profiles             {:dev {:dependencies [[swank-clojure "1.4.3"]
+                                              [lein-marginalia "0.6.0"]]}
+                         :1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
+                         :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+                         :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+                         :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+                         :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :dependencies [[org.clojure/java.classpath "0.2.0"]
+                 [org.clojure/core.incubator "0.1.0"]
+                 [org.clojure/data.json      "0.2.0"]
+                 [org.clojure/tools.logging  "0.2.3"]
+                 [commons-io                 "2.0"]
+                 [log4j/log4j                "1.2.17"]
+                 [org.mindrot/jbcrypt        "0.3m"]
+                 [commons-httpclient         "3.1"]
+                 [commons-codec              "1.8"]
+                 [commons-lang               "2.6"]
+                 [joda-time/joda-time        "2.3"]]
   :autodoc {
     :name        "clj-etl-utils"
     :page-title  "clj-etl-utils: API Documentation"
