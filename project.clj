847a8fc79fa5632f4e6bfac1d61e11991c5eab76
Kyle Burton <kyle.burton@gmail.com>
2013-09-15 13:04:58 -0400

    lein-release plugin: bumped version from 1.0.76 to 1.0.77-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.kyleburton/clj-etl-utils "1.0.76"
+(defproject com.github.kyleburton/clj-etl-utils "1.0.77-SNAPSHOT"
   :description "ETL Utilities"
   :url         "http://github.com/kyleburton/clj-etl-utils"
   :lein-release {:deploy-via :clojars :scm :git}
