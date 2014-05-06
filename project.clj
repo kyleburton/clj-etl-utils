b067a42a43e80dacdb1f8a73ae119190a879c6ce
Kyle Burton <kyle.burton@gmail.com>
2014-05-06 11:09:37 -0400

    use 1.6.0 now that it is released, exclude clojure from clj-time dep

--- project.clj
+++ project.clj
@@ -26,7 +26,7 @@
                                               [org.clojure/data.json      "0.2.3"]]}
                          :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]
                                               [org.clojure/data.json      "0.2.3"]]}
-                         :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]
+                         :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]
                                               [org.clojure/data.json      "0.2.3"]]}}
   :aliases              {"all" ["with-profile" "dev,1.2:dev,1.3:dev,1.4:dev,1.5:dev,1.6"]}
   :global-vars          {*warn-on-reflection* true}
@@ -38,4 +38,5 @@
                          [commons-lang               "2.5"]
                          [org.clojure/data.csv       "0.1.2"]
                          [org.clojure/tools.logging  "0.2.6"]
-                         [clj-time                  "0.7.0"]])
+                         [clj-time                  "0.7.0"
+                           :exclusions [org.clojure/clojure]]])
