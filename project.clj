c508f6692514a51f2f8bc48f39343afcbe5fa269
Kyle Burton <kyle.burton@gmail.com>
2014-08-22 11:09:18 -0400

    lein-release plugin: preparing 1.0.92 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.kyleburton/clj-etl-utils "1.0.92-SNAPSHOT"
+(defproject com.github.kyleburton/clj-etl-utils "1.0.92"
   :description "ETL Utilities"
   :url         "http://github.com/kyleburton/clj-etl-utils"
   :lein-release {:deploy-via :clojars :scm :git}
